cb5c875affdf49ae1e191f6236ea753925206c26
animalize <malincns@163.com>
2018-03-16 20:50:15 +0800

    栈1

--- app/src/main/java/com/github/animalize/ting/MainListActivity.java
+++ app/src/main/java/com/github/animalize/ting/MainListActivity.java
@@ -58,7 +58,6 @@ public class MainListActivity
         }
     };
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/github/animalize/ting/TingTTS/TingTTSService.java
+++ app/src/main/java/com/github/animalize/ting/TingTTS/TingTTSService.java
@@ -1,11 +1,8 @@
 package com.github.animalize.ting.TingTTS;
 
 import android.app.Notification;
-import android.app.PendingIntent;
-import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
-import com.github.animalize.ting.MainListActivity;
 import com.github.animalize.ting.R;
 import com.github.animalize.ting.TTS.Setting;
 import com.github.animalize.ting.TTS.TTSService;
@@ -24,16 +21,11 @@ public class TingTTSService extends TTSService {
     @Override
     public void doStartForeground() {
         // 前台服务
-        Intent notificationIntent = new Intent(this, MainListActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(
-                this, 0, notificationIntent, 0);
-
         Notification notification = new NotificationCompat.Builder(
                 this, "828")
                 .setSmallIcon(R.drawable.notifiy_icon)
                 .setContentTitle("Ting的前台服务")
-                .setContentText("")
-                .setContentIntent(pendingIntent)
+                .setContentText("保持TTS服务持续运行")
                 .build();
         startForeground(828, notification);
     }
