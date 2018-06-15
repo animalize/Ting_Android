33953a8c755b6b7815e976a4e98039a785e1e035
animalize <malincns@163.com>
2018-06-15 11:28:10 +0800

    播放时卡片改变背景色 3

--- app/src/main/java/com/github/animalize/ting/Data/MyColors.java
+++ app/src/main/java/com/github/animalize/ting/Data/MyColors.java
@@ -10,5 +10,5 @@ public class MyColors {
     public static final int current = Color.rgb(0xff, 0x00, 0x00);
 
     public static final int cardStop = Color.rgb(0xcc, 0xcb, 0x9e);
-    public static final int cardPlaying = Color.rgb(0xc2, 0x7b, 0xa0);
+    public static final int cardPlaying = Color.rgb(0xd5, 0xa6, 0xbd);
 }
